619431d647563eecd21d001ec61cf5d026536450
Devang Kamble <risingentropy20@gmail.com>
2021-01-21 21:40:00 +0530

    Base html

--- WCECourses/WCECourses/urls.py
+++ WCECourses/WCECourses/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.landingPage.as_view(), name="landingPage"),
     path('register', views.register.as_view(), name="register"),
     path('login', views.Login.as_view(), name="Login"),
+    path('logout', views.logout_user, name='logout'),
 ]
