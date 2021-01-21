7b4e671da5326bb780cac4346ba1747fd83c71bc
Devang Kamble <risingentropy20@gmail.com>
2021-01-21 16:36:25 +0530

    Files

---
+++ WCECourses/WCECourses/views.py
@@ -0,0 +1,91 @@
+import os
+from django.contrib.auth import authenticate, login, logout
+from django.views import generic
+from django.views.generic.base import View
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+from student.models import student as Student
+
+class landingPage(View):
+    
+    def get(self, request, template_name='landingPage.html'):
+        return render(request, template_name)
+
+class register(View):
+
+    def get(self, request, template_name='register.html'):
+        return render(request, template_name)
+
+    def post(self, request, template_name='register.html'):
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        designation = request.POST.get('designation')
+        organisation = request.POST.get('organisation')
+        address = request.POST.get('address')
+        phoneNumber = request.POST.get('phoneNumber')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confPassword = request.POST.get('conf_password')
+
+        if password != confPassword:
+            err = {'error_message': "Passwords don't match. Please Try Again."}
+            return render(request, 'student/stureg.html', err)
+
+        try:
+            user = User.objects.create_user(username, email, password, first_name=first_name,
+                                            last_name=last_name, )
+            user.save()
+        except:
+            err = {}
+            err['error_message'] = "Account with this Username or Email already Exists."
+            return render(request, template_name, err)
+
+        try:
+            studentData = Student(user=user, designation=designation, organisation=organisation, address=address, phoneNumber=phoneNumber)
+            studentData.save()
+        except:
+            err = {}
+            err['error_message'] = "Account with this Username already Exists."
+            return render(request, template_name, err)
+
+        my_group = Group.objects.get(name='student_group')
+        my_group.user_set.add(user)
+
+        err = {}
+        err['error_message'] = "Registration Successful. Please Login."
+        return render(request, template_name, err)
+
+class Login(View):
+
+    def get(self, request, template_name='login.html'):
+        return render(request, template_name)
+
+    def post(self, request, template_name='login.html'):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        group = None
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                userr = Group.objects.filter(user=user)
+                group = user.groups.all()[0].name
+                if group == 'faculty_group':
+                    return redirect('teacher:facultyHome')
+                if group == 'student_group':
+                    return redirect('student:studentHome')
+                else:
+                    return render(request, 'landingPage.html', {})
+            else:
+                return render(request, 'login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid login'})
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return render(request, 'landingPage.html', {})
---
+++ WCECourses/student/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+User._meta.get_field('email')._unique = True
+from django.utils import timezone
+import datetime
+from django.core.validators import MaxValueValidator
+
+# Create your models here.
+from django.db.models import OneToOneField
+
+class student(models.Model):
+    user: OneToOneField = models.OneToOneField(User, on_delete=models.CASCADE)
+    designation = models.CharField(max_length=20)
+    organisation = models.CharField(max_length=100)
+    address = models.CharField(max_length=400)
+    phoneNumber = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.user.username
